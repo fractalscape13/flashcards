b289b3b8f75f91148ff2b9bd4ca9470451b93166
Ian Cook Westgate and Joseph Wangemann <joseph@>
2019-01-15 14:30:30 -0800

    It will habe all buttons working absolutely.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,125 @@
 $(function() {
-  $(".clickable").click(function() {
+  $(".clickable1").click(function() {
     $(".definition").toggle();
-    $(".showdefinition").toggle();
+    $(".showdefinition1").toggle();
+  });
+});
+
+$(function() {
+  $(".clickable2").click(function() {
+    $(".definition").toggle();
+    $(".showdefinition2").toggle();
+  });
+});
+
+$(function() {
+  $(".clickable3").click(function() {
+    $(".definition").toggle();
+    $(".showdefinition3").toggle();
+  });
+});
+
+$(function() {
+  $(".clickable4").click(function() {
+    $(".definition").toggle();
+    $(".showdefinition4").toggle();
+  });
+});
+
+$(function() {
+  $(".clickable5").click(function() {
+    $(".definition").toggle();
+    $(".showdefinition5").toggle();
+  });
+});
+
+$(function() {
+  $(".clickable6").click(function() {
+    $(".definition").toggle();
+    $(".showdefinition6").toggle();
+  });
+});
+
+$(function() {
+  $(".clickable7").click(function() {
+    $(".definition").toggle();
+    $(".showdefinition7").toggle();
+  });
+});
+
+$(function() {
+  $(".clickable8").click(function() {
+    $(".definition").toggle();
+    $(".showdefinition8").toggle();
+  });
+});
+
+$(function() {
+  $(".clickable9").click(function() {
+    $(".definition").toggle();
+    $(".showdefinition9").toggle();
+  });
+});
+
+$(function() {
+  $(".clickable10").click(function() {
+    $(".definition").toggle();
+    $(".showdefinition10").toggle();
+  });
+});
+
+$(function() {
+  $(".clickable11").click(function() {
+    $(".definition").toggle();
+    $(".showdefinition11").toggle();
+  });
+});
+
+$(function() {
+  $(".clickable12").click(function() {
+    $(".definition").toggle();
+    $(".showdefinition12").toggle();
+  });
+});
+
+$(function() {
+  $(".clickable13").click(function() {
+    $(".definition").toggle();
+    $(".showdefinition13").toggle();
+  });
+});
+
+$(function() {
+  $(".clickable14").click(function() {
+    $(".definition").toggle();
+    $(".showdefinition14").toggle();
+  });
+});
+
+$(function() {
+  $(".clickable15").click(function() {
+    $(".definition").toggle();
+    $(".showdefinition15").toggle();
+  });
+});
+
+$(function() {
+  $(".clickable16").click(function() {
+    $(".definition").toggle();
+    $(".showdefinition16").toggle();
+  });
+});
+
+$(function() {
+  $(".clickable17").click(function() {
+    $(".definition").toggle();
+    $(".showdefinition17").toggle();
+  });
+});
+
+$(function() {
+  $(".clickable18").click(function() {
+    $(".definition").toggle();
+    $(".showdefinition18").toggle();
   });
 });
